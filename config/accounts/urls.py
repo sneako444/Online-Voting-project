85fae61efd164144e3dbb37a9fb4b0590d9c6b50
sam <samgeorge1621@gmail.com>
2025-04-12 16:28:43 +0530

    test

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import register_view, login_view, dashboard_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('dashboard/', dashboard_view, name='dashboard'),  # Dashboard page
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('voting/', TemplateView.as_view(template_name="accounts/voting.html"), name='voting'),
 ]
--- config/elections/urls.py
+++ config/elections/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import list_elections, election_detail, vote
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', list_elections, name='list_elections'),
     path('<int:election_id>/', election_detail, name='election_detail'),
     path('vote/<int:candidate_id>/', vote, name='vote'),
+    path('accounts/', include('accounts.urls')),  # Include accounts app URLs
+    path('', RedirectView.as_view(url='/accounts/dashboard/', permanent=False)),  # Redirect root to dashboard
 ]
